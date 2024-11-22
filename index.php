7277532665c1042f97c3b556b5e104bfcc91155e
elchonger <lukeattack@hotmail.com>
2024-11-22 10:47:38 +0000

    Shop link added

--- index.php
+++ index.php
@@ -55,6 +55,7 @@
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
                         <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                        <!-- <li class="nav-item"><a class="nav-link" href="#shop">Shop</a></li> -->
                         <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                     </ul>
                 </div>
